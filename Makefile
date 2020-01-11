91aab699f11ada48df5005b5a4b1ef1f41c743ad
elad11310 <elad11310@Gmail.com>
2020-01-11 15:45:00 +0200

    upload assignment 4

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC=gcc
+AR=ar
+
+FLAGS= -Wall -g
+
+all:  frequecny	
+
+frequecny: TrieFunc.o main.o
+	$(CC)  $(FLAGS) -o frequecny main.o TrieFunc.o
+
+main.o: main.c Trie.h  
+	$(CC) $(FLAGS) -c main.c 
+
+
+TrieFunc.o: TrieFunc.c Trie.h
+	$(CC) $(FLAGS) -c TrieFunc.c
+
+
+
+.PHONY: frequecny clean all 
+
+clean:
+	rm -f *.o *.a *.so  frequecny 
---
+++ main.c
@@ -0,0 +1,55 @@
+#include "Trie.h"
+int maxSize=0;
+int main() {
+
+
+
+	node* root = getNewTrieNode(' '); // initializing the root
+	char c=0; 
+	char* str= NULL;
+	Node* head = NULL;
+	int count = 0;
+
+	while (((c = getc(stdin)) != EOF)) {
+		if (c != ' ' && c!='\n') {
+			count++;
+			if(c<97 || c>122){ // if c is not small letter
+				c=c+'a'-'A';
+			}
+			head = add(head, c);
+		}
+		
+		if (c == ' ' || c=='\n') {
+			if(maxSize<count){
+			maxSize=count; // saving the maximum length of word 						//that we get
+			}
+			str = fromListToStr(head,str,count);
+					
+					insert(root, str);
+					 head =freeList(head);
+					free(str);
+					count = 0;
+
+
+
+
+		}
+
+		
+
+
+	}
+	
+					
+	free(str);
+	printTreeLexUp(root);
+	printf("------\n");
+	printTreeLexDown(root);
+	freeTree(root);
+	free(root);
+	root=NULL;
+
+
+
+	return 0;
+}
